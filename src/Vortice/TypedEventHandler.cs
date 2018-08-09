f2043a617c451905ac80a14028e8363172179725
Amer Koleci <amerkoleci@gmail.com>
2018-08-09 20:49:32 +0200

    Initial solution and build support.

---
+++ src/Vortice/TypedEventHandler.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Amer Koleci and contributors.
+// Licensed under the MIT License.
+
+namespace Vortice
+{
+    /// <summary>
+    /// Defines a method that handles general events.
+    /// </summary>
+    /// <typeparam name="TSender">The typed sender type.</typeparam>
+    /// <param name="sender">The event source.</param>
+    public delegate void TypedEventHandler<TSender>(TSender sender);
+
+    /// <summary>
+    /// Defines a method that handles general events.
+    /// </summary>
+    /// <typeparam name="TSender">The typed sender type.</typeparam>
+    /// <typeparam name="TResult">The typed event data.</typeparam>
+    /// <param name="sender">The event source.</param>
+    /// <param name="args">The event data. If there is no event data, this parameter will be null.</param>
+    public delegate void TypedEventHandler<TSender, TResult>(TSender sender, TResult args);
+}
